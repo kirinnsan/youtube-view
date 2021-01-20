1187a33000fc64603d4f938d8107e1166bfc19bd
Hiroshi Takaichi <hirosi.t822@gmail.com>
2021-01-20 15:18:41 +0000

    Add api and modify function name

--- client.py
+++ client.py
@@ -11,10 +11,10 @@ class ApiClient(object):
         self.service = build(YOUTUBE_API_SERVICE_NAME,
                              YOUTUBE_API_VERSION, credentials=credential)
 
-    def get_videos_list(self, video_id):
+    def fetch_videos_list(self, video_id: str):
         """
         YouTube動画IDに一致する動画情報を取得。
-        idパラメータには、取得するリソースのYouTube動画IDをカンマ区切りリストの形式で指定。
+        idパラメータは、カンマ区切りで複数指定可能。
         """
         try:
             result = self.service.videos().list(
@@ -22,25 +22,45 @@ class ApiClient(object):
                 part="snippet,contentDetails,statistics"
             ).execute()
         except HttpError as err:
-            print(f'action=get_videos_list error={err}')
+            print(f'action=fetch_videos_list error={err}')
             raise
 
         return result
 
-    def get_youtube_channels_list(self):
+    def fetch_video_categories_list(self):
+        """
+        ビデオカテゴリ情報を取得。
+
+        Note:
+            regionCodeに、ISO 3166-1 alpha-2 の国コードを指定すると、
+            指定された国で使用できる動画カテゴリのリストを返す。
+        """
         try:
-            # 自身のチャンネルリストを取得。
+            result = self.service.videoCategories().list(
+                part="snippet",
+                regionCode="JP"
+            ).execute()
+        except HttpError as err:
+            print(f'action=fetch_video_categories_list error={err}')
+            raise
+
+        return result
+
+    def fetch_youtube_channels_list(self):
+        """自身のチャンネルリストを取得。"""
+        try:
             result = self.service.channels().list(
                 mine=True,
                 part="id,snippet,contentDetails"
             ).execute()
         except HttpError as err:
-            print(f'action=get_youtube_channels_list error={err}')
+            print(f'action=fetch_youtube_channels_list error={err}')
             raise
 
         return result
 
-    def get_youtube_playlists_list(self, list_id):
+    def fetch_youtube_playlists_list(self):
+        """自身の再生リストを取得。"""
         try:
             result = self.service.playlists().list(
                 mine=True,
@@ -48,7 +68,7 @@ class ApiClient(object):
                 maxResults=50
             ).execute()
         except HttpError as err:
-            print(f'action=get_youtube_playlists error={err}')
+            print(f'action=fetch_youtube_playlists_list error={err}')
             raise
 
         return result
