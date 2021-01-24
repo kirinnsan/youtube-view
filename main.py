77f0e59ca47968dcf9287a205a9a6a2d8489a70a
Hiroshi Takaichi <hirosi.t822@gmail.com>
2021-01-24 05:53:40 +0000

    Delete unnecessary files

--- main.py
+++ main.py
@@ -112,10 +112,6 @@ def load_watch_history(start_date):
     # 今月視聴したデータ取得
     df_range = df[df['time'] > datetime(
         start_date.year, start_date.month, 1, tzinfo=timezone.utc)]
-    # TODO 動作確認のため、2021年1月10日以降のデータを対象
-    # today = datetime.today()
-    # df_range = df[df['time'] > datetime(
-    #     today.year, today.month, 10, tzinfo=timezone.utc)]
 
     result = df_range[['title', 'titleUrl', 'time']]
     result = result.values.tolist()
